3fc82be8de9ee46ebf7c9a58ff36f0e8131e56f0
zyfneverforget <262829233@qq.com>
2018-12-04 17:46:42 +0800

    fix swiper

--- src/pages/home/components/HomeSwiper.vue
+++ src/pages/home/components/HomeSwiper.vue
@@ -1,11 +1,8 @@
 <template>
 	<div class="wrapper">
 		<swiper :options="swiperOption">
-			<swiper-slide>
-				<img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1811/4d/6706bf772c05ce02.jpg_750x200_773e75f0.jpg" alt="">
-			</swiper-slide>
-			<swiper-slide>
-				<img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg" alt="">
+			<swiper-slide v-for="item in list" :key ="item.id">
+				<img class="swiper-img" :src="item.imgUrl" alt="">
 			</swiper-slide>
 			<div class="swiper-pagination"  slot="pagination"></div>
 		</swiper>
@@ -21,7 +18,15 @@
         paginationClickable: true,
 				autoplay: 3000,
 				loop: true,
-			}
+			},
+			list: [
+				{
+					id: '0001',
+					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/4d/6706bf772c05ce02.jpg_750x200_773e75f0.jpg'
+				},{
+					id: '0002',
+					imgUrl: 'http://img1.qunarzz.com/piao/fusion/1811/7c/8e5c4ab8ee8b7402.jpg_750x200_dd7a38dd.jpg'
+				}]
 		}
 	}
  }
